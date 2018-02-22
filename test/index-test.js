23d79521d62a7206b976fec94972e7cf10c55e5f
alexbman30@yahoo.com <alexbman30@yahoo.com>
2018-02-22 01:20:44 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -65,8 +65,10 @@ function sayHiToGrandma(string){
   return(`I can\'t hear you!`)
 }
 function sayHiToGrandma(string){
-  return("YES INDEED!")
+  return(`YES INDEED!`)
 }
+
 function sayHiToGrandma(string){
-  return("I love you, too.")
+  return(`I love you, too.`)
 }
+
